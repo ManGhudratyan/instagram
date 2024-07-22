3c818e8f85e858b05f0e3ac29737f9aeafae29a0
Mane <mane.ghudratyan@gmail.com>
2024-07-22 15:52:16 +0400

    Chat is ready now

--- lib/data/services/chat/chat_service_imp.dart
+++ lib/data/services/chat/chat_service_imp.dart
@@ -1,51 +1,3 @@
-// import 'package:firebase_database/firebase_database.dart';
-
-// import 'chat_service.dart';
-
-// class ChatServiceImp implements ChatService {
-//   final DatabaseReference _database = FirebaseDatabase.instance.ref();
-
-//   @override
-//   Future<void> sendMessage(
-//       String senderId, String receiverId, String messageText) async {
-//     final timeSend = DateTime.now().toUtc().toIso8601String();
-//     final messageRef =
-//         _database.child('users/$senderId/chats/$receiverId/messages').push();
-//     final message = {
-//       'text': messageText,
-//       'senderId': senderId,
-//       'timeSend': timeSend,
-//     };
-//     await messageRef.set(message);
-
-//     _database
-//         .child('users/$receiverId/chats/$senderId/messages')
-//         .push()
-//         .set(message);
-//   }
-
-//   @override
-//   Future<List<Map<String, dynamic>>> loadMessages(
-//       String senderId, String receiverId) async {
-//     final DatabaseEvent event = await _database
-//         .child('users/$senderId/chats/$receiverId/messages')
-//         .orderByChild('timeSend')
-//         .once();
-
-//     final snapshot = event.snapshot;
-//     if (snapshot.value == null) {
-//       return [];
-//     }
-
-//     final messagesMap = snapshot.value as Map<Object?, Object?>;
-//     final messagesList = messagesMap.entries.map((entry) {
-//       final value = entry.value as Map<Object?, Object?>;
-//       return value.map((key, value) => MapEntry(key.toString(), value));
-//     }).toList();
-
-//     return messagesList;
-//   }
-// }
 
 import 'package:firebase_database/firebase_database.dart';
 
@@ -82,7 +34,9 @@ class ChatServiceImp implements ChatService {
         .onValue
         .map((event) {
       final snapshot = event.snapshot;
-      if (snapshot.value == null) return [];
+      if (snapshot.value == null) {
+        return [];
+      }
 
       final messagesMap = snapshot.value! as Map<Object?, Object?>;
       return messagesMap.entries.map((entry) {
